7fdb8355b907b5719e4f084a4f6583e0995d6336
konst1988 <konst1988@gmail.com>
2016-07-05 16:22:29 +0300

    Solution for: sol-python-verint.syntax-lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -5,3 +5,12 @@ For example if the number was: 2345
 The result should be: 14
 """
 
+from random import randint
+num = randint(1,10000)
+print "num is:{}".format(num)
+sum = 0
+while num > 0:
+    digit = num % 10
+    sum += digit
+    num /= 10;
+print "sum of digits is:{}".format(sum)
