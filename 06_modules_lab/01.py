540da7b962ad821db9c405cd04a7d8f66945b209
konst1988 <konst1988@gmail.com>
2016-07-06 14:40:48 +0300

    Solution for: sol-python-verint.modules-lab/01.py

--- 06_modules_lab/01.py
+++ 06_modules_lab/01.py
@@ -2,3 +2,13 @@
 from sys.argv import and prints "Hello Python"
 count times
 """
+
+import sys
+
+if len(sys.argv) > 1:
+    count = sys.argv[1]
+    for i in range(int(count)):
+        print "Hello Python"
+else:
+    program_name = sys.argv[0]
+    print "Usage: %s <count>" % program_name
