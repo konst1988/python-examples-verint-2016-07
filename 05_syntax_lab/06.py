15dc0eba5bd6766494ccdb0eadbd864fa8cea5c6
konst1988 <konst1988@gmail.com>
2016-07-06 11:52:40 +0300

    Solution for: sol-python-verint.syntax-lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -7,3 +7,12 @@ For example if the numbers were 4 and 6,
 program should print 12
 """
 
+from random import randint
+
+num1 = randint(1,10)
+num2 = randint(1,10)
+
+for i in range(num1, num1*num2+1):
+    if i % num1 == 0 and i % num2 == 0:
+        print "{} is divisbale by {} and {}".format(i,num1,num2)
+        break;
