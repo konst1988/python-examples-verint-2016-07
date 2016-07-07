5210ec255046fe473405e51a5c45581debadf6df
konst1988 <konst1988@gmail.com>
2016-07-07 09:17:10 +0300

    Solution for: sol-python-verint.modules-lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -5,7 +5,7 @@ and prints their sum.
 import sys
 
 if len(sys.argv) > 2:
-    (name, strnum1, strnum2) = sys.argv
+    (_, strnum1, strnum2) = sys.argv
     num1 = int(strnum1)
     num2 = int(strnum2)
     print "{} + {} = {}".format(num1, num2, num1+num2)
