080dc453feb90570bbcf16b49340b6447940ca0c
konst1988 <konst1988@gmail.com>
2016-07-10 17:16:51 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -4,3 +4,12 @@ and appends the second file's contents to
 the end of the first one
 """
 
+import sys
+
+(src, dst) = sys.argv[1:]
+
+with open(src, "r") as fin:
+    with open(dst, "a") as fout:
+        for line in fin:
+            fout.write(line.title())
+            
