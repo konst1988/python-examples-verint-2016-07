03f570b283bbc1966074108501dd30a091edb710
konst1988 <konst1988@gmail.com>
2016-07-13 15:54:45 +0300

    Solution for: sol-python-verint.files-lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -4,4 +4,18 @@ are existing file names and the last is a new file name
 Program should write the lines from the first two 
 files interwinded into the output file
 """
+import itertools
+import sys
+
+(_,srcA, srcB, dst) = sys.argv
+
+with open(srcA, "r") as a:
+    with open(srcB, "r") as b:
+        with open(dst, "w") as c:
+            for item in itertools.izip_longest(a,b, fillvalue=""):
+                c.write(item[0])
+                c.write(item[1])
+
+
+
 
