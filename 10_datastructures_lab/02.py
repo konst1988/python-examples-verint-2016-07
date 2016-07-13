c220c58ae367bbd606edd34d2d0b58cf760726de
konst1988 <konst1988@gmail.com>
2016-07-13 12:15:31 +0300

    Solution for: sol-python-verint.datastructures-lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -1,5 +1,21 @@
-"""
-Write a program that prints only unique values
-from sys.argv (without program name)
-"""
+from collections import Counter
+import sys
+
+anagrams = []
+#read all words from file
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+       word = line[:-1]
+       wordCount = Counter(line[:-1])
+       
+       # see if same counter already appeared
+       found = False
+       for anagram in anagrams:
+           if anagram[0] == wordCount :
+               found = True
+               print "{} {}".format(word, anagram[1])
+
+        # Add to list if not found
+       if not found:
+            anagrams.append((wordCount, word))
 
