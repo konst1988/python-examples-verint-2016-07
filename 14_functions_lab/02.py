ff874eeef82fea5aa20bd47f5cf860fe1dfd61c9
konst1988 <konst1988@gmail.com>
2016-07-14 14:48:52 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -4,4 +4,11 @@ Write a function that takes two arguments:
     And a number
 If wrong types were passed in, raise an exception
 """
+import numbers
+
+def test(str,num):
+    if not isinstance(str, basestring): raise Exception("str must be string")
+    if not isinstance(num ,numbers.Number) : raise Exception("num must be a number")
+
+test("uy","uu")
 
