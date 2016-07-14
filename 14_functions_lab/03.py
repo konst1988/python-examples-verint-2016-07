4af7133d4936d7621861488d51ccbb76f3b1200f
konst1988 <konst1988@gmail.com>
2016-07-14 14:55:48 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -3,3 +3,7 @@ Write a function that calculates the sum
 of the 10th digit from all arguments passed to it
 """
 
+def sum10th(*nums):
+    return sum([int(str(x)[-2]) for x in nums])
+
+print sum10th(1120, 220, 140)
