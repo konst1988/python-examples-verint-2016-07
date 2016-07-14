b6bb21ca650a86b1b0f99a6f831d0d95a22c75e8
konst1988 <konst1988@gmail.com>
2016-07-14 15:09:17 +0300

    Solution for: sol-python-verint.functions-lab/04.py

--- 14_functions_lab/04.py
+++ 14_functions_lab/04.py
@@ -3,4 +3,7 @@ Write a function that takes minlen and
 a list of words, and returns only the words
 longer than minlen
 """
+def longer_than(length, *strings):
+    return [s for s in strings if len(s) > length ]
 
+print longer_than(3, 'hit', 'me', 'baby', 'one', 'more', 'time')
