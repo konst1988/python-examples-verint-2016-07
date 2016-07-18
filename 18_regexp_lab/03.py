e454943ac4a4d2962c60b951fae7ac7c9ff6f1a4
konst1988 <konst1988@gmail.com>
2016-07-18 17:10:30 +0300

    Solution for: sol-python-verint.regexp-lab/03.py

--- 18_regexp_lab/03.py
+++ 18_regexp_lab/03.py
@@ -13,3 +13,16 @@ Sample output:
     Hampton,Witt,witthampton@zaphire.com
     Grant,Morgan,morgangrant@lotron.com
 """
+
+import sys
+import re
+
+(_,filename) = sys.argv
+
+with open(filename, "r") as f:
+    for line in f:
+        fields = re.search(r'^(.*),(.*),(.*)$',line)
+        if fields == None:
+            print "invalid line '{}'".format(line)
+            continue
+        print "{},{},{}".format(fields.group(2),fields.group(1), fields.group(3))
