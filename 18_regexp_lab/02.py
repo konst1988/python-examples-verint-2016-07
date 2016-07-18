0293f7bd05d82a528b7b886aa4232473f086757d
konst1988 <konst1988@gmail.com>
2016-07-18 16:53:20 +0300

    Solution for: sol-python-verint.regexp-lab/02.py

--- 18_regexp_lab/02.py
+++ 18_regexp_lab/02.py
@@ -3,4 +3,14 @@ Write a function toCamelCase(text) that returns the text camel cased
 Write a function to_underscore(text) that returns the text with 
 underscores between words
 """
+import re
 
+def toCamelCase(text):
+    return re.sub(r'_(\w)',
+        lambda m: m.group(1).upper()
+        , text)
+
+print toCamelCase('welcome')
+print toCamelCase('hello_world')
+print toCamelCase('get_name')
+print toCamelCase('no_more_shall_we_part')
