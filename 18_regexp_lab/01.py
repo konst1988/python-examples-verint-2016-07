4a162476f9962d147ffbea8629994814bbef7b89
konst1988 <konst1988@gmail.com>
2016-07-18 14:50:33 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -9,4 +9,17 @@ Each line in the file can either be:
 Write a program that takes a property file name and key
 as command line arguments and prints the requested value
 """
+import sys
+import re
 
+(_,filename,key) = sys.argv
+
+with open(filename, "r") as f:
+    for line in f:
+        if line[0] == '\n' or line[0] == '#': continue
+        keyvalue = re.search(r'^(\w+)[ ]*=[ ]*(\w+)$',line)
+        if keyvalue == None:
+            print "'{}' is not a key value line".format(line)
+        elif keyvalue.group(1) == key:
+            print "{} is {}".format(keyvalue.group(1), keyvalue.group(2))
+    print "key '{}' not found".format(key)
