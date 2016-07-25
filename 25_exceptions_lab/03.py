2c46fb246559acc44b16d0d0f0265ba45fd6c6a5
konst1988 <konst1988@gmail.com>
2016-07-25 16:11:29 +0300

    Solution for: sol-python-verint.exceptions-lab/03.py

--- 25_exceptions_lab/03.py
+++ 25_exceptions_lab/03.py
@@ -2,3 +2,34 @@
 Write code to make the following unit test pass
 """
 
+import unittest
+import re
+import sys
+
+class InvalidImageExt(Exception):
+
+    def __init__(self, file):
+        super(InvalidImageExt, self).__init__("File '{}' has an invalid image extension".format(file))
+
+
+class ImageFile(object):
+
+    def __init__(self, file):
+        if not re.search(r'[.]gif|png|jpg|jpeg$', file):
+            raise InvalidImageExt(file)
+        self._file = file
+
+
+class TestImageFile(unittest.TestCase):
+
+    def test_good_ext(self):
+        try:
+            img = ImageFile("file.png")
+        except InvalidImageExt:
+            self.fail("png should be a valid file extension")
+
+    def test_bad_ext(self):
+        with self.assertRaises(InvalidImageExt):
+            img = ImageFile("file.mp3")
+
+unittest.main()
