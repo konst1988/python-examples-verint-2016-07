919f2aa893d727bcb3728b0b9a562d5d019b4db5
konst1988 <konst1988@gmail.com>
2016-07-25 15:40:56 +0300

    Solution for: sol-python-verint.exceptions-lab/01.py

--- 25_exceptions_lab/01.py
+++ 25_exceptions_lab/01.py
@@ -4,4 +4,16 @@ and for each number prints its square root
 If value is negative or not a number show 
 a warning and keep reading values
 """
+import sys
+import math
 
+while True:
+    number = raw_input("Enter number:")
+    if number == 0: sys.exit()
+    try:
+        result = math.sqrt(int(number))
+        print "SQRT of {} is {}".format(number, result)
+    except ValueError as e:
+        print "Invalid number for sqrt. Error: {}".format(e.message)
+    except Exception as e:
+        print "Unknown Error occurred: {}".format(e.message)
