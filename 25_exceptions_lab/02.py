fcc475d1c54e175d938f684ed5b74a14260ab26d
konst1988 <konst1988@gmail.com>
2016-07-25 15:50:40 +0300

    Solution for: sol-python-verint.exceptions-lab/02.py

--- 25_exceptions_lab/02.py
+++ 25_exceptions_lab/02.py
@@ -4,4 +4,14 @@ and prints line count for the file
 
 Alert the user politely if there was any problem opening the file
 """
+import sys
 
+file = sys.argv[1]
+
+try:
+    with open(file, "rb") as fin:
+        print (len(fin.readlines()))
+except IOError as e:
+    print "Failed to count lines of file: '{}'. Reason: {}".format(e.filename,e.strerror)
+except Exception as e:
+    print "Unknown Error occurred: {}".format(e.message)
